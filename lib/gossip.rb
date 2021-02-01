12d7f304652071ddbe81f68b44b3456a26cb8295
frederic <fredericseguier@yahoo.fr>
2021-02-01 20:38:59 +0100

    the gossip avec sinatra by fred

---
+++ lib/gossip.rb
@@ -0,0 +1,43 @@
+#require 'pry'
+require 'csv'
+
+class Gossip
+  attr_accessor :author, :content
+
+  def initialize(author, content)
+    @author = author
+    @content = content
+  end
+
+  def save
+    CSV.open("./db/gossip.csv", "ab") do |csv|
+      csv << [@author, @content]
+    end
+  end
+
+  def self.all
+    all_gossips = []
+    CSV.read("./db/gossip.csv").each do |csv_line|
+      all_gossips << Gossip.new(csv_line[0], csv_line[1])
+    end
+    return all_gossips
+  end
+
+  def self.find(id)
+    return CSV.read("./db/gossip.csv")[id]
+  end
+
+  def self.update(id, new_author, new_content)
+    all_gossips = self.all
+    all_gossips[id].author = new_author
+    all_gossips[id].content = new_content
+    CSV.open("./db/gossip.csv", "w") do |csv|
+      all_gossips.each do |gossip|
+        csv << [gossip.author, gossip.content]
+      end
+    end
+  end
+
+end
+
+#binding.pry
